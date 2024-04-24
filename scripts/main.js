017463ca58a63c91817268d832fbe9a0e496b587
oasoby <134684352+oasoby@users.noreply.github.com>
2024-04-24 15:52:34 +0900

    1.20.80.xに完全対応

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { Vector, system, world } from "@minecraft/server";
+import { system, world } from "@minecraft/server";
 
 const overworld = world.getDimension("overworld");
 
@@ -30,61 +30,27 @@ const scales = [
     "F#",
 ]
 
-world.afterEvents.itemUseOn.subscribe(ev => {
-    const { source, itemStack, block } = ev;
-    if (!source.isSneaking) {
-        if (itemStack.typeId == "note:note_stick" && block.typeId == "minecraft:noteblock") {
-            const blocklocation = block.location;
-            system.run(() => {
-                overworld.runCommandAsync(`structure load __noteblocks ${blocklocation.x} 319 ${blocklocation.z}`);
-
-                system.run(() => {
-                    try {
-                        const chestInv = overworld.getBlock(new Vector(blocklocation.x, 319, blocklocation.z)).getComponent("minecraft:inventory").container;
-                        chestInv.addItem(block.getItemStack(1, true));
-                        for (let i = 0; i < chestInv.size; i++) {
-                            const item = chestInv.getSlot(i);
-                            if (1 < item.amount) {
-                                source.onScreenDisplay.setActionBar(`scale: ${scales[i]} click: ${i}`);
-                                break;
-                            }
-                        }
-                    } catch (err) {
-                        console.warn(err);
-                    }
-                    overworld.runCommandAsync(`setblock ${blocklocation.x} 319 ${blocklocation.z} air`);
-                })
-            })
-        }
-    }
-})
-
-world.beforeEvents.itemUseOn.subscribe(ev => {
-    const { source, itemStack, block } = ev;
+world.beforeEvents.itemUseOn.subscribe(e => {
+    const { source, itemStack, block } = e;
     if (source.isSneaking) {
-        ev.cancel = true;
-        if (itemStack.typeId == "note:note_stick" && block.typeId == "minecraft:noteblock") {
-            const blocklocation = block.location;
+        e.cancel = true;
+    }
+    if (itemStack.typeId == "note:note_stick" && block.typeId == "minecraft:noteblock") {
+        const blocklocation = block.location;
+        system.run(() => {
+            overworld.runCommandAsync(`structure load __noteblocks ${blocklocation.x} 319 ${blocklocation.z}`);
             system.run(() => {
-                overworld.runCommandAsync(`structure load __noteblocks ${blocklocation.x} 319 ${blocklocation.z}`);
-
-                system.run(() => {
-                    try {
-                        const chestInv = overworld.getBlock(new Vector(blocklocation.x, 319, blocklocation.z)).getComponent("minecraft:inventory").container;
-                        chestInv.addItem(block.getItemStack(1, true));
-                        for (let i = 0; i < chestInv.size; i++) {
-                            const item = chestInv.getSlot(i);
-                            if (1 < item.amount) {
-                                source.onScreenDisplay.setActionBar(`scale: ${scales[i]} click: ${i}`);
-                                break;
-                            }
-                        }
-                    } catch (err) {
-                        console.warn(err);
+                const chestInv = overworld.getBlock({ x: blocklocation.x, y: 319, z: blocklocation.z }).getComponent("minecraft:inventory").container;
+                chestInv.addItem(block.getItemStack(1, true));
+                for (let i = 0; i < chestInv.size; i++) {
+                    const item = chestInv.getSlot(i);
+                    if (1 < item.amount) {
+                        source.onScreenDisplay.setActionBar(`scale: ${scales[i]} click: ${i}`);
+                        break;
                     }
-                    overworld.runCommandAsync(`setblock ${blocklocation.x} 319 ${blocklocation.z} air`);
-                })
+                }
+                overworld.runCommandAsync(`setblock ${blocklocation.x} 319 ${blocklocation.z} air`);
             })
-        }
+        })
     }
 })
