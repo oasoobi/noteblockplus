771dbbc1db8ae384c43aa5be50ef131b714a987a
oasoby <134684352+oasoby@users.noreply.github.com>
2024-12-02 14:59:24 +0900

    1.21.50対応

--- scripts/main.js
+++ scripts/main.js
@@ -385,9 +385,9 @@ system.afterEvents.scriptEventReceive.subscribe(e => {
         })
     } else if (id == "note:version") {
         if (sourceEntity.getDynamicProperty("language") == 1) {
-            sourceEntity.sendMessage("§eNoteBlock+のバージョンは 2.0.3 です。");
+            sourceEntity.sendMessage("§eNoteBlock+のバージョンは 2.0.4 です。");
         } else {
-            sourceEntity.sendMessage("§eNoteBlock+ is at version 2.0.3.");
+            sourceEntity.sendMessage("§eNoteBlock+ is at version 2.0.4.");
         }
     }
 })
