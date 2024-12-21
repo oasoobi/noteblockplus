d45ea0c080a19242b802b0f548df86564d54a157
oasoby <134684352+oasoby@users.noreply.github.com>
2024-12-21 10:19:11 +0900

    ファイルを分割

---
+++ scripts/datalist.js
@@ -0,0 +1,213 @@
+export const Instruments = {
+    "lit_pumpkin": "piano", //上書き回避用
+    "carved_pumpkin": "piano", //上書き回避用
+    "redstone_block": "piano", //上書き回避用
+    "_leaves": "piano", //葉系
+    "noteblock": "bass",
+    "bookshelf": "bass",
+    "creaking_heart": "bass", //クリーキングの心臓
+    "pale_oak": "bass", //ペールオーク系
+    "oak": "bass", //オークの木材系
+    "spruce": "bass", //トウヒ系
+    "birch": "bass", //樺系
+    "jungle": "bass", //ジャングル系
+    "acacia": "bass", //アカシア系
+    "dark_oak": "bass", //ダークオーク系
+    "cherry": "bass", //桜系
+    "bamboo": "bass", //竹系
+    "crimson": "bass", //クリムゾン系
+    "warped": "bass", //歪んだ系
+    "mangrove": "bass", //マングローブ系
+    "chest": "bass", //チェスト系
+    "crafting_table": "bass",
+    "jukebox": "bass",
+    "mushroom": "bass", //キノコ系
+    "banner": "bass", //旗系
+    "daylight_detector": "bass",
+    "loom": "bass",
+    "barrel": "bass",
+    "cartography_table": "bass",
+    "lectern": "bass",
+    "smithing_table": "bass",
+    "fletching_table": "bass",
+    "campfire": "bass", //焚き火系
+    "composter": "bass",
+    "bee_nest": "bass",
+    "beehive": "bass",
+    "amethyst_block": "bass_drum", //アメジスト系
+    "soul_sand": "cow_bell",
+    "glass": "clicks_and_sticks", //ガラス系
+    "sea_lantern": "clicks_and_sticks",
+    "beacon": "clicks_and_sticks",
+    "infested_": "flute", //虫食いブロック系
+    "glowstone": "pling_piano",
+    "end_portal_frame": "bass_drum",
+    "stone": "bass_drum", //石系
+    "andesite": "bass_drum", //安山岩系
+    "granite": "bass_drum", //花崗岩系
+    "diorite": "bass_drum", //閃緑岩系
+    "basalt": "bass_drum", //玄武岩系
+    "calcite": "bass_drum",
+    "purpur": "bass_drum", //プルプァ系
+    "terracotta": "bass_drum", //色付き、彩釉テラコッタ系
+    "quartz": "bass_drum", //クォーツブロック系
+    "tuff": "bass_drum", //凝灰岩系
+    "blackstone": "bass_drum", //ブラックストーン系
+    "netherrack": "bass_drum",
+    "nylium": "bass_drum", //ナイリウム系
+    "obsidian": "bass_drum", //黒曜石系
+    "sandstone": "bass_drum", //砂岩系
+    "_ore": "bass_drum", //鉱石系
+    "deepslate": "bass_drum", //深層岩系
+    "hardened_clay": "bass_drum", //通常のテラコッタ
+    "clay": "flute", //粘土
+    "raw_": "bass_drum", //原石ブロック系
+    "brick": "bass_drum", //レンガ系
+    "prismarine": "bass_drum", //プリズマリン系
+    "coral": "bass_drum", //サンゴ系
+    "respawn_anchor": "bass_drum",
+    "bedrock": "bass_drum",
+    "observer": "bass_drum",
+    "monster_spawner": "bass_drum",
+    "concrete_powder": "snare_drum", //コンクリートパウダー系 上書き回避のため、コンクリートより優先度を上げる
+    "concrete": "bass_drum", //コンクリート系
+    "gold_block": "bells",
+    "honeycomb_block": "flute",
+    "monster_egg": "flute",
+    "infested": "flute",
+    "resin": "flute", //レジン系
+    "packed_ice": "chimes",
+    "wool": "guitar",
+    "bone_block": "xylophone",
+    "iron_block": "iron_xylophone",
+    "pumpkin": "didgeridoo",
+    "emerald_block": "bit",
+    "hay_block": "banjo",
+    "sand": "snare_drum", //砂、赤砂、怪しげな砂
+    "gravel": "snare_drum" //砂利、怪しげな砂利
+}
+
+export const InstrumentsTranslateKey = {
+    ja: {
+        "piano": "ピアノ",
+        "bass": "バス",
+        "bass_drum": "バスドラム",
+        "cow_bell": "カウベル",
+        "clicks_and_sticks": "スティック",
+        "flute": "フルート",
+        "pling_piano": "電子ピアノ",
+        "snare_drum": "スネアドラム",
+        "bells": "ベル",
+        "chimes": "チャイム",
+        "guitar": "ギター",
+        "xylophone": "木琴",
+        "iron_xylophone": "鉄琴",
+        "didgeridoo": "ディジュリドゥ",
+        "bit": "電子音",
+        "banjo": "バンジョー"
+    },
+    en: {
+        "piano": "Piano",
+        "bass": "Bass",
+        "bass_drum": "Bass Drum",
+        "cow_bell": "Cow Bell",
+        "clicks_and_sticks": "Clicks and Sticks",
+        "flute": "Flute",
+        "pling_piano": "Pling Piano",
+        "snare_drum": "Snare",
+        "bells": "Bells",
+        "chimes": "Chimes",
+        "guitar": "Guitar",
+        "xylophone": "Xylophone",
+        "iron_xylophone": "Iron Xylophone",
+        "didgeridoo": "Didgeridoo",
+        "bit": "Bit",
+        "banjo": "Banjo"
+    }
+}
+
+export const Scales = {
+    international: [
+        "F#",
+        "G",
+        "G#",
+        "A",
+        "A#",
+        "B",
+        "C",
+        "C#",
+        "D",
+        "D#",
+        "E",
+        "F",
+        "F#",
+        "G",
+        "G#",
+        "A",
+        "A#",
+        "B",
+        "C",
+        "C#",
+        "D",
+        "D#",
+        "E",
+        "F",
+        "F#"
+    ],
+    solfege: {
+        en: [
+            "Fa#",
+            "Sol",
+            "Sol#",
+            "La",
+            "La#",
+            "Si",
+            "Do",
+            "Do#",
+            "Re",
+            "Re#",
+            "Mi",
+            "Fa",
+            "Fa#",
+            "Sol",
+            "Sol#",
+            "La",
+            "La#",
+            "Si",
+            "Do",
+            "Do#",
+            "Re",
+            "Re#",
+            "Mi",
+            "Fa",
+            "Fa#"
+        ],
+        ja: [
+            "ファ#",
+            "ソ",
+            "ソ#",
+            "ラ",
+            "ラ#",
+            "シ",
+            "ド",
+            "ド#",
+            "レ",
+            "レ#",
+            "ミ",
+            "ファ",
+            "ファ#",
+            "ソ",
+            "ソ#",
+            "ラ",
+            "ラ#",
+            "シ",
+            "ド",
+            "ド#",
+            "レ",
+            "レ#",
+            "ミ",
+            "ファ",
+            "ファ#",
+        ]
+    }
+}
